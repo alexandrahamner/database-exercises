688de75d0e70d1b7ecf160f4a4321c611e9812f9
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-26 14:04:04 -0500

    feat: created the albums_migration.sql script for the tables exercise. Completed the exercise.

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(40) NOT NULL,
+    album_name VARCHAR(40) NOT NULL,
+    release_date DATE,
+    sales DECIMAL(9,2),
+    genre VARCHAR(20),
+    PRIMARY KEY (id)
+);
+DESCRIBE albums;
