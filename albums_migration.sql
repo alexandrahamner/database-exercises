a2d9d9deb4a6085217a2e0e8c49b7e30a2e75b82
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-27 11:01:57 -0500

    feat: created albums_seeder sql file to add a list of values to the albums table.

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(40) NOT NULL,
-    album_name VARCHAR(40) NOT NULL,
-    release_date DATE,
+    album_name VARCHAR(70) NOT NULL,
+    release_date INT UNSIGNED,
     sales FLOAT,
-    genre VARCHAR(20),
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 );
 DESCRIBE albums;
