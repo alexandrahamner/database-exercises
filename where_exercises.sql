82eedaba864d5e500eaaa22c5d976b92b1830463
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-28 11:08:27 -0500

    feat: completed the second part of the where exercise.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,16 +1,22 @@
 USE employees;
 
 SELECT first_name, last_name FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE gender = 'm'
+   AND (first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya');
 
 SELECT first_name, last_name FROM employees
-WHERE last_name LIKE 'e%';
-
-SELECT first_name, last_name, hire_date FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01';
-
-SELECT first_name, last_name, birth_date FROM employees
-WHERE birth_date LIKE '%%%%-12-25';
+WHERE last_name LIKE 'e%'
+    OR last_name LIKE '%e';
 
 SELECT first_name, last_name FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE 'e%e';
+
+SELECT first_name, last_name, hire_date, birth_date FROM employees
+WHERE birth_date LIKE '%%%%-12-25'
+    AND hire_date > '1990-01-01';
+
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
