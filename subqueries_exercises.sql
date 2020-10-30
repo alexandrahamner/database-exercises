843c1e5c85b775467324451dec326313560da517
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-29 21:33:29 -0500

    feat: made a few changes to better match the exercise.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -16,8 +16,8 @@ WHERE emp_no IN (
     WHERE first_name = 'Aamod'
 );
 
-SELECT first_name, last_name, emp_no, gender
-FROM employees as e
+SELECT first_name, last_name
+FROM employees
 WHERE emp_no IN(
     SELECT emp_no
     FROM dept_manager
